9d46e0415cb8c0f6db1115a2958c390c63503eb2
QNeee <vovasagan7@gmail.com>
2022-11-26 05:17:16 +0300

    dsda

--- src/index.js
+++ src/index.js
@@ -2,29 +2,33 @@ import { fetchMovies } from "./fetch";
 let q = '';
 const refs = {
     form: document.querySelector("#search-form"),
-    movieGallery: document.querySelector(".galleryMovie"),
+    movieGallery: document.querySelector(".movieGallery"),
     button: document.querySelector("#clearButton")
 }
 
 function createMarkup(data) {
     return data.reduce((acc, { Title, Poster, Type, Year, imdbID }) =>
-        `${acc}<tr class="tr">
+        `${acc}
     <td>${Title}</td>
     <td>${Year}</td>
     <td>${Type}</td>
-  </tr>`, "");
+  `, "");
 }
 function onSubmit(e) {
     e.preventDefault();
     q = e.target.searchQuery.value.trim();
     if (!q) return;
     fetchMovies(q).then(({ data }) => {
-        addMarkup(data.Search);
+        if (data.length > 0) {
+            createMarkup(data.Search);
+        }
     });
 }
 
 function addMarkup(data) {
+
     return refs.movieGallery.innerHTML = createMarkup(data);
+
 }
 refs.form.addEventListener("submit", onSubmit);
 
