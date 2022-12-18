0d9883cb64b2d5da06c2ecacad8ad2fcbe3c635f
QNeee <vovasagan7@gmail.com>
2022-12-18 19:34:30 +0300

    dsadas

--- src/index.js
+++ src/index.js
@@ -2,26 +2,28 @@ import { fetchMovies } from "./fetch";
 let q = '';
 const refs = {
     form: document.querySelector("#search-form"),
-    movieGallery: document.querySelector(".movieGallery"),
+    movieGallery: document.querySelector(".galleryMovie"),
     button: document.querySelector("#clearButton")
 }
 
 function createMarkup(data) {
     return data.reduce((acc, { Title, Poster, Type, Year, imdbID }) =>
-        `${acc}
-    <td>${Title}</td>
-    <td>${Year}</td>
-    <td>${Type}</td>
-  `, "");
+        `${acc}<li><div>
+    <p>${Title}</p>
+    <p>${Year}</p>
+    <p>${Type}</p>
+    <p><img src ="${Poster}"/></p>
+    </div></li>
+  `, " ");
 }
 function onSubmit(e) {
     e.preventDefault();
     q = e.target.searchQuery.value.trim();
     if (!q) return;
     fetchMovies(q).then(({ data }) => {
-        if (data.length > 0) {
-            addMarkup(data.Search);
-        }
+        // console.log(data.Search);
+        addMarkup(data.Search);
+
     });
 }
 
